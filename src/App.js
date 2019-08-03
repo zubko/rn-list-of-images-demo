0c2677552953d5753a0169ae3f977d94ce7464e9
Alexander Zubko <alexander.zubko@gmail.com>
2019-08-03 18:34:15 +0300

    ➕Add emotion

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
+import styled, { css } from '@emotion/native';
+
+const Description = styled.Text({
+  color: 'hotpink',
+});
+
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+        <Description>Home Screen</Description>
       </View>
     );
   }
